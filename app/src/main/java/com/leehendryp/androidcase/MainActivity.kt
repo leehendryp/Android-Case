d3559f81e4f6aecdf702f0f251bcfda586112a59
leehendryp <leehendryp@gmail.com>
2020-03-24 14:38:22 -0300

    feat(permissions): add runtime permission dialog for location

--- app/src/main/java/com/leehendryp/androidcase/MainActivity.kt
+++ app/src/main/java/com/leehendryp/androidcase/MainActivity.kt
@@ -1,12 +1,52 @@
 package com.leehendryp.androidcase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat.checkSelfPermission
+import androidx.core.app.ActivityCompat.requestPermissions
+import com.leehendryp.androidcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        getLocationPermission()
+    }
+
+    private fun getLocationPermission() {
+        if (checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String?>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            REQUEST_LOCATION_PERMISSION ->
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getLocationPermission()
+                }
+        }
+    }
+
+    companion object {
+        private const val REQUEST_LOCATION_PERMISSION = 0
     }
 }
