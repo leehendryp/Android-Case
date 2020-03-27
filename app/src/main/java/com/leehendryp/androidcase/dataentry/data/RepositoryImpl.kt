7a3bdcf157419ebed998e2f1b0880cdd61a5c169
leehendryp <leehendryp@gmail.com>
2020-03-27 10:38:05 -0300

    feat(repository): draft implementation based on abstractions

---
+++ app/src/main/java/com/leehendryp/androidcase/dataentry/data/RepositoryImpl.kt
@@ -0,0 +1,35 @@
+package com.leehendryp.androidcase.dataentry.data
+
+import com.leehendryp.androidcase.core.utils.coTryCatch
+import com.leehendryp.androidcase.dataentry.data.entities.request.InfoForAntt
+import com.leehendryp.androidcase.dataentry.data.entities.request.InfoProvidedByDriver
+import com.leehendryp.androidcase.dataentry.data.local.LocalDataSource
+import com.leehendryp.androidcase.dataentry.data.remote.RemoteDataSource
+import com.leehendryp.androidcase.dataentry.domain.Repository
+import com.leehendryp.androidcase.dataentry.domain.RouteWithAnttPrices
+import com.leehendryp.androidcase.dataentry.domain.createRouteWithAnttPrices
+
+class RepositoryImpl(
+    private val remoteSource: RemoteDataSource,
+    private val localSource: LocalDataSource
+) : Repository {
+    override suspend fun getRouteDetailsFrom(info: InfoProvidedByDriver): RouteWithAnttPrices = coTryCatch {
+        val routeDetails = remoteSource.getRouteDetailsFrom(info)
+        val anttPrices = remoteSource.getAnttPrices(
+            InfoForAntt(
+                axis = info.shafts,
+                distance = routeDetails.distance?.toDouble() ?: 0.0,
+                hasReturnShipment = true
+            )
+        )
+        createRouteWithAnttPrices(routeDetails, anttPrices)
+    }
+
+    override suspend fun save(routeWithAnttPrices: RouteWithAnttPrices) = coTryCatch {
+        localSource.save(routeWithAnttPrices)
+    }
+
+    override suspend fun getHistory(): Set<RouteWithAnttPrices> = coTryCatch {
+        localSource.getHistory()
+    }
+}
