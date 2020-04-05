5bf6c5e75a2eb6964298ef82fea7eb1db06a3a40
leehendryp <leehendryp@gmail.com>
2020-04-05 18:10:23 -0300

    feat(security): secure google cloud platform key

--- app/src/main/java/com/leehendryp/androidcase/core/AndroidCaseApplication.kt
+++ app/src/main/java/com/leehendryp/androidcase/core/AndroidCaseApplication.kt
@@ -1,11 +1,19 @@
 package com.leehendryp.androidcase.core
 
 import android.app.Application
+import com.google.android.libraries.places.api.Places
+import com.leehendryp.androidcase.BuildConfig
 import com.leehendryp.androidcase.core.di.AppComponent
 import com.leehendryp.androidcase.core.di.DaggerAppComponent
 
 class AndroidCaseApplication : Application() {
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(applicationContext)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Places.initialize(this, BuildConfig.GOOGLE_CLOUD_KEY)
+        Places.createClient(this)
     }
 }
