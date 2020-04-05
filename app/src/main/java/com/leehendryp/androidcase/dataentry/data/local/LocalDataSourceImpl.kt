4b76577df6a074bf8ebdea8960b9f89b0d30eac8
leehendryp <leehendryp@gmail.com>
2020-04-05 17:01:11 -0300

    feat(local-source): add shell for satisfying dependency injection

---
+++ app/src/main/java/com/leehendryp/androidcase/dataentry/data/local/LocalDataSourceImpl.kt
@@ -0,0 +1,13 @@
+package com.leehendryp.androidcase.dataentry.data.local
+
+import com.leehendryp.androidcase.dataentry.domain.RouteWithAnttPrices
+
+class LocalDataSourceImpl : LocalDataSource {
+    override suspend fun save(routeWithAnttPrices: RouteWithAnttPrices) {
+
+    }
+
+    override suspend fun getHistory(): Set<RouteWithAnttPrices> {
+        return setOf()
+    }
+}
